92daea5715e34fcb2bca81c918637ed61d0b28cc
Lartey Joshua <joshua@raspberrypi>
2019-07-17 15:58:07 +0000

    Simple Todo Build with Flask

--- myapp.py
+++ myapp.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
  
+items =[]
 app = Flask(__name__)
  
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('todolist.html', to=name,items=items)
 
- 
+    
+
+@app.route('/add_todo')
+def add_todo():
+    item =request.args.get('item')
+    items.append(item)
+    return redirect("http://localhost:5000/foo/joshua", code=302)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
